540622bc48f2f4fd2e07fd11fa837485513950eb
ramtob <ramtob@gmail.com>
2019-01-14 14:38:30 +0200

    initial commit, changes vis a vis angular-ansyn-implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import {Component} from '@angular/core';
-import {AnsynApi} from '@ansyn/ansyn';
-import {LayoutKey, layoutOptions} from '@ansyn/core';
-import {FeatureCollection} from 'geojson';
-import {HttpClient} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { AnsynApi } from '@ansyn/ansyn';
+import { HttpClient } from '@angular/common/http';
+import { take, tap } from 'rxjs/operators';
+import * as momentNs from 'moment';
 
-import {Observable} from 'rxjs';
+const moment = momentNs;
+
+
+interface IDilutedOverlay {
+  id: string;
+  sourceType?: string;
+}
+
+interface IOverlay extends IDilutedOverlay {
+  footprint?: any; // @TODO add type geojson multipoligon,
+  sensorType?: string;
+  sensorName?: string;
+  channel?: number;
+  bestResolution?: number;
+  cloudCoverage?: number;
+  isStereo?: boolean;
+  name: string;
+  imageUrl?: string;
+  baseImageUrl?: string;
+  thumbnailUrl?: string;
+  photoTime: string;
+  date: Date;
+  azimuth: number; // radians
+  approximateTransform?: any;
+  csmState?: string;
+  isGeoRegistered: boolean;
+  tag?: any; // original metadata
+  projection?: string;
+  token?: string;
+  catalogID?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,41 +42,77 @@ import {Observable} from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  layoutKeys: LayoutKey[] = Array.from(layoutOptions.keys());
-  featureOptions: FeatureCollection<any>;
+  overlays = [
+    this.overlay('000', 'https://upload.wikimedia.org/wikipedia/commons/e/e2/Reeipublic_Banana.gif',
+      576, 1024, this.calcDate(0)),
+    this.overlay('111', 'https://image.shutterstock.com/image-vector/cool-comic-book-bubble-text-450w-342092249.jpg',
+      470, 450, this.calcDate(1)),
+    this.overlay('222', 'https://imgs.xkcd.com/comics/online_communities.png',
+      1024, 968, this.calcDate(2)),
+    this.overlay('333', 'https://image.shutterstock.com/z/stock-vector-cool-milkshake-190524542.jpg',
+      1600, 1500, this.calcDate(3))
+  ];
+
+  calcDate(days: number) {
+    return moment().subtract(days, 'day').toDate();
+  }
+
+  overlay(id: string, imageUrl: string, imageWidth: number, imageHeight: number, date: Date): IOverlay {
+    const left = -117.94,
+      top = 33.82,
+      width = 0.05,
+      height = 0.02,
+      right = left + width * Math.random(),
+      bottom = top - height * Math.random();
+    return {
+      name: id,
+      id: id,
+      photoTime: date.toISOString(),
+      date: date,
+      azimuth: 0,
+      isGeoRegistered: false,
+      sourceType: 'STATIC_IMAGE',
+      tag: {
+        imageData: {
+          imageWidth: imageWidth,
+          imageHeight: imageHeight
+        }
+      },
+      footprint: {
+        type: 'MultiPolygon',
+        coordinates: [[[
+          [left, top],
+          [right, top],
+          [right, bottom],
+          [left, bottom],
+          [left, top]
+        ]
+        ]
+        ]
+      },
+      baseImageUrl: '',
+      imageUrl: imageUrl,
+      thumbnailUrl: imageUrl,
+      sensorName: 'mySensorName',
+      sensorType: 'mySensorType',
+      bestResolution: 1,
+      cloudCoverage: 1
+    };
+  }
 
   constructor(protected ansynApi: AnsynApi,
               private http: HttpClient) {
-
-    this.getFeatureCollectionFromFile().subscribe(response => {
-      this.featureOptions = response.featureCollection;
-    });
-    this.ansynApi.getActiveCenter$.subscribe((center) => {
-      console.log(center);
-    });
   }
 
-  setFeatureCollection(): void {
-    this.ansynApi.setAnnotations(this.featureOptions);
+  displayOverlaysOnTwoMaps() {
+    this.ansynApi.changeMapLayout('layout2').pipe(
+      tap(() => {
+        this.ansynApi.setOverlays(this.overlays);
+        this.ansynApi.displayOverlayOnMap(this.overlays[1], 0);
+        this.ansynApi.displayOverlayOnMap(this.overlays[2], 1);
+      }),
+      take(1)
+    ).subscribe();
   }
 
-  deleteFeatureCollection(): void {
-    this.ansynApi.deleteAllAnnotations();
-  }
-
-  setLayout(layout: LayoutKey): void {
-    this.ansynApi.changeMapLayout(layout);
-  }
-
-  getFeatureCollectionFromFile(): Observable<any> {
-    return this.http.get('assets/featureCollection.json');
-  }
-
-  setPositionWithRadius() {
-    this.ansynApi.setMapPositionByRadius({type: 'Point', coordinates: [-117.914, 33.8117]}, 100, true);
-  }
-
-  clearOverlays() {
-    this.ansynApi.setOverlays([]);
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
-import {fetchConfigProviders} from '@ansyn/ansyn';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { fetchConfigProviders } from '@ansyn/ansyn';
 
 if (environment.production) {
   enableProdMode();
 }
 
-fetchConfigProviders('assets/app.config.json')
+const mergeChanges = {
+  'casesConfig': {
+    'defaultCase': {
+      'state': {
+        'facets': {
+          'showOnlyFavorites': false,
+          'filters': [
+            {
+              'metadata': {
+                'displayTrue': true,
+                'displayFalse': true
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  'coreConfig': {
+    'noInitialSearch': true
+  }
+};
+
+fetchConfigProviders('assets/app.config.json', mergeChanges)
   .then(providers => platformBrowserDynamic(providers).bootstrapModule(AppModule).catch(err => console.log(err)));
